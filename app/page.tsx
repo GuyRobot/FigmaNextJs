e01f49750a05e0ecd7d393fd54c27b6635aa6272
GuyRobot <bluexker@gmail.com>
2024-02-17 20:43:19 +0700

    Add styles and assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { Room } from "./Room";
 import { CollaborativeApp } from "./CollaborativeApp";
 
 export default function Page() {
-  return (
-    <Room>
-      <CollaborativeApp />
-    </Room>
-  );
+  return <CollaborativeApp />;
 }
