35259f6c9b6d42fcc521ed859cbc33453838ef33
Guys Robot <51730903+GuyRobot@users.noreply.github.com>
2024-02-21 01:10:43 +0000

    Add avatars on nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import Navbar from "@/components/Navbar";
 import Live from "@/components/cursor/Live";
 
 export default function Page() {
   return (
-    <div className="">
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex flex-row h-full">
+        <Live />
+      </section>
+    </main>
   );
 }
